8c583256c4ee27ded3c2cf164268eea8117e910d
Fernando Jose Delgado-Quintana <fernandojosesu@users.noreply.github.com>
2017-02-05 22:04:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# GentleGeek Studio
+- [GentleGeek Studio](http://www.gentlegeekstudios.com/) Portfolio Page.
+- Previously known as FJDQ Designs.
---
+++ assets/js/scripts.js
@@ -0,0 +1,63 @@
+/*
+ Author: Fernando J. Delgado Quintana
+ Template: Washer (Coming Soon)
+ Version: 1.0
+ */
+
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+    /*
+     ----------------------------------------------------------------------
+     Preloader
+     ----------------------------------------------------------------------
+     */
+    $(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+
+
+    /*
+     ----------------------------------------------------------------------
+     Nice scroll
+     ----------------------------------------------------------------------
+     */
+    $("html").niceScroll({
+        cursorcolor: '#fff',
+        cursoropacitymin: '0',
+        cursoropacitymax: '1',
+        cursorwidth: '2px',
+        zindex: 999999,
+        horizrailenabled: false,
+        enablekeyboard: false
+    });
+
+
+    /*
+     ----------------------------------------------------------------------
+     Watch
+     ----------------------------------------------------------------------
+     */
+    if($.find('#watch')[0]) {
+
+        $('#watch').countDown({
+            targetDate: {
+                'day': 		20,
+                'month': 	6,
+                'year': 	2016,
+                'hour': 	12,
+                'min': 		0,
+                'sec': 		0
+            },
+            omitWeeks: true
+        });
+        //enter the count down date using the format year, month, day, time: hour, min, sec
+
+        if( $('.day_field .top').html() == "0" ) $('.day_field').css('display','none');
+
+    }
+
+
+}); // End $(document).ready(function(){
